a25582e3c01911c2a01b759e6da38076a7e2c4d4
Alex Noppenberger <noppenbe@gmx.de>
2022-05-07 20:23:48 +0200

    no registration on strato

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                        @if (Route::has('register'))
+                        @if (Route::has('register') && $_SERVER['HTTP_HOST']=='lara.test')
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
