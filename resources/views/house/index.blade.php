5ca6386308509fab3368d567dbc798ccc8efd293
Alex <noppenbe@gmx.de>
2022-10-07 17:24:32 +0200

    Update index.blade.php

--- resources/views/house/index.blade.php
+++ resources/views/house/index.blade.php
@@ -10,7 +10,11 @@
         <div class="w-full mx-auto px-6">
             <div class="bg-white w-full overflow-hidden shadow-xl sm:rounded-lg grid grid-cols-4">
                 @foreach ($houses as $house)
-                    <div class="p-2"><a href="{{ route('house.edit', ['id' => $house->id] ) }}">{{ $house->name }}</a></div>
+                        <div class="p-2">
+                            <a href="{{ route('house.edit', ['id' => $house->id] ) }}">
+                                {{ $house->house_number }}
+                            </a>
+                        </div>
                         <div class="p-2">{{ $house->detail }}</div>
                         <div class="p-2">{{ $house->created_at }}</div>
                         <div class="p-2">{{ $house->updated_at }}</div>
