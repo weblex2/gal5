b0130757ad6c6b67d0a5633469b5236e1ebb0f58
Alex Noppenberger <noppenbe@gmx.de>
2022-10-15 12:51:35 +0200

    Update GalleryController.php

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -55,7 +55,6 @@ class GalleryController extends Controller
             echo "nöö";
             die();
         }
-        echo storage_path('app/public/gal/1/a.jpg');
         #chmod(storage_path('app/public/gal/1/'),770);
         try{
             $video = $ffmpeg->open($url);
@@ -63,9 +62,10 @@ class GalleryController extends Controller
             #    ->filters()
             #    ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
             #    ->synchronize();
-            $video
-                ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(1))
-                ->save(storage_path('app/public/gal/1/a.jpg'));
+            $frame = $video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(1)); 
+            #$video
+            #    ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(1))
+            $frame->save(storage_path('app/public/gal/1/a.jpg'));
         #$video
         #    ->save(new FFMpeg\Format\Video\X264(), storage_path('app/public/gal/1/export-x264.mp4'))
         #    ->save(new FFMpeg\Format\Video\WMV(),  storage_path('app/public/gal/1/export-wmv.wmv'))
